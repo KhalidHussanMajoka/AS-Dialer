e8e3b4ffe366d8d4902c5889bb6060255750ab98
KhalidHussanMajoka <108335078+KhalidHussanMajoka@users.noreply.github.com>
2022-11-04 08:53:15 -0700

    contact page weekend

--- www.dialer360.com/contact-us.php
+++ www.dialer360.com/contact-us.php
@@ -7,19 +7,21 @@
     <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
       <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">Contact Us</h2>
       <p class="leading-relaxed mb-5 text-gray-600">AS-Dialer</p>
-      <div class="relative mb-4">
-        <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
-        <input type="name" id="name" name="name" class="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out">
-      </div>
-      <div class="relative mb-4">
-        <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
-        <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out">
-      </div>
-      <div class="relative mb-4">
-        <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
-        <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 h-32 text-base outline-none text-gray-700 py-5 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
-      </div>
-      <button class="text-white bg-yellow-500 border-0 py-6 px-6 focus:outline-none hover:bg-yellow-600 rounded text-lg">Button</button>
+        <form action="mailto:Info@asdialer.com" method="post">
+           <div class="relative mb-4">
+             <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+             <input type="name" id="name" name="name" class="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out">
+           </div>
+           <div class="relative mb-4">
+             <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+             <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 text-base outline-none text-gray-700 py-2 px-3 leading-8 transition-colors duration-200 ease-in-out">
+           </div>
+           <div class="relative mb-4">
+             <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+             <textarea id="message" name="message" class="w-full bg-white rounded border border-gray-300 focus:border-yellow-500 focus:ring-2 focus:ring-yellow-200 h-32 text-base outline-none text-gray-700 py-5 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+           </div>
+           <button class="text-white bg-yellow-500 border-0 py-6 px-6 focus:outline-none hover:bg-yellow-600 rounded text-lg">Button</button>
+        </form>
       <p class="text-xs text-gray-500 mt-3">Feel free to contact us anytime.</p>
     </div>
   </div>
